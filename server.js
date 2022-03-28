68cfea0f5dc3d3baf606d18f0567e69ae0591f63
haipv <>
2022-03-28 08:33:29 +0700

    add PORT

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const users_routes = require('./routes/users')
 require('dotenv').config()
 
 mongoose.connect(process.env.MONGO_URI)
-    .then((result) => app.listen(5000))
+    .then((result) => app.listen(process.env.PORT || 5000))
     .catch((err) => console.log(Error))
 
 app.use(express.json())
